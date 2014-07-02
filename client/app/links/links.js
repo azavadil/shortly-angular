f4d820fa587a9b9cdcd3aa59ee5988b0e0ce0599
Hack Reactor Students <anthony.zavadil@gmail.com>
2014-07-02 16:46:14 -0700

    implement links template

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -1,5 +1,19 @@
 angular.module('shortly.links', [])
 
-.controller('LinksController', function ($scope, Links) {
+.controller('LinksController', function ($scope, Links, $http) {
   $scope.data = {};
+  $scope.getLinks = function(){
+    $http({
+      method:'GET',
+      url: '/api/links'
+    })
+    .then(function(response){
+      var dataFromServer = response.data;
+      $scope.data.links = dataFromServer;
+    })
+    .catch(function(error){
+      console.log(error);
+    });
+  };
+  $scope.getLinks();
 });
